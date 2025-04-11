39679d913e8bc2e10f60142b02f296dbfe0f19e1
SumS <suman.sahana98@gmail.com>
2025-04-11 09:46:35 +0530

    Extent Reports and screenshots capture added in MagentoSTB Project

--- MagentoSTB/src/test/java/test/EndToEndTest.java
+++ MagentoSTB/src/test/java/test/EndToEndTest.java
@@ -1,5 +1,11 @@
 package test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,6 +15,8 @@ import pages.Homepage;
 import utils.ConfigReader;
 import utils.ExcelUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 
@@ -17,6 +25,18 @@ public class EndToEndTest {
     WebDriver driver;
     WebDriverWait wait;
     pages.Homepage home;
+
+    ExtentReports extent;
+    ExtentTest test;
+
+
+    @BeforeSuite
+    public void setUpExtentReport(){
+        ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+
+    }
 
 
     @BeforeMethod
@@ -43,7 +63,7 @@ public class EndToEndTest {
 
 
     @Test(dataProvider = "excelTests")
-    public void endToEndTest(String testCaseID,String testCaseName){
+    public void endToEndTest(String testCaseID,String testCaseName) throws IOException, InterruptedException {
         System.out.println("Running Test --->"+testCaseID+"::"+testCaseName);
         switch (testCaseID){
             case "TC001":
@@ -58,19 +78,37 @@ public class EndToEndTest {
 
     }
 
-    private void custLoginTest() {
-        //nested POM homepage ----> CustomerLoginPage
-        CustomerLogin custLogin = home.signIn();
-        custLogin.setEmail(ConfigReader.properties.getProperty("email"));
-        custLogin.setPass(ConfigReader.properties.getProperty("pass"));
-        //nested POM CustomerLoginPage ----> HomePage
-        home = custLogin.clickLogin();
-        home.signOut();
+    private void custLoginTest()  {
+        try{
+            test = extent.createTest("custLoginTest");
+            //nested POM homepage ----> CustomerLoginPage
+            CustomerLogin custLogin = home.signIn();
+            custLogin.setEmail(ConfigReader.properties.getProperty("email"));
+            custLogin.setPass(ConfigReader.properties.getProperty("pass"));
+            //nested POM CustomerLoginPage ----> HomePage
+            home = custLogin.clickLogin();
+            Thread.sleep(800);
+
+            //Take Screenshot
+            File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(src,new File("screenshots/LoggedIn.png"));
+            test.pass("Successfull Login");
+
+            home.signOut();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @AfterMethod
     public void tearDown(){
         driver.close();
         driver.quit();
+    }
+
+    @AfterSuite
+    public void flushReports(){
+        extent.flush();
     }
 }
